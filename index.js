93393e099c2ecc2aca306e172ff0ea1ff7e88757
Ritsuki Goto <develop@mizucoffee.net>
2019-01-17 16:11:42 +0900

    [add] font

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.post('/convert', async (req, res) => {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   const page = await browser.newPage()
   await page.goto(`http://localhost:3000/${id}.html`)
+  await page.evaluateHandle('document.fonts.ready');
 
   const clip = await page.evaluate(s => {
     const { width, height, top: y, left: x } = document.querySelector(s).getBoundingClientRect()
